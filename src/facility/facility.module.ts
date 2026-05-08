1d82c61bd3eaceab081ae613a3556da08bc16965
ashraf571 <ashrafali.gcs@gmail.com>
2026-05-08 12:29:35 +0500

    create get all facilities api

--- src/facility/facility.module.ts
+++ src/facility/facility.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FacilityService } from './facility.service';
 import { FacilityController } from './facility.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Facility } from './entities/facility.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Facility])],
   controllers: [FacilityController],
   providers: [FacilityService],
 })
-export class FacilityModule {}
+export class FacilityModule { }
--- src/facility/facility.service.ts
+++ src/facility/facility.service.ts
@@ -1,15 +1,73 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFacilityDto } from './dto/create-facility.dto';
 import { UpdateFacilityDto } from './dto/update-facility.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Facility } from './entities/facility.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class FacilityService {
+  constructor(
+    @InjectRepository(Facility)
+    private readonly facilityRepository: Repository<Facility>,
+  ) { }
   create(createFacilityDto: CreateFacilityDto) {
     return 'This action adds a new facility';
   }
 
-  findAll() {
-    return `This action returns all facility`;
+
+  private facilityProperties(facility: Facility) {
+    return {
+      id: facility.facility_id,
+      title: facility.name,
+      area: facility.totalarea,
+      numberoffloors: facility.numberoffloors,
+      numberofoffices: facility.numberofoffices,
+      numberofshops: facility.numberofshops,
+      areaunit: facility.areaunit,
+      description: facility.description,
+      dimensions: facility.dimensions,
+      elevations: facility.elevations,
+      // stairs: facility.stairs,
+      // type: facility.type,
+      // areaunit: level.areaunit,
+      // dimensions: level.dimensions,
+      // description: level.description,
+    }
+  }
+
+  private toGeoJSON(rows: any[]) {
+    return {
+      type: "FeatureCollection",
+      name: "facility-geojson",
+
+      features: rows.map((row) => ({
+        type: "Feature",
+        properties: this.facilityProperties(row),
+        geometry: row.geometry,
+      }))
+
+    };
+  }
+
+  async findAll() {
+    const facilites = await this.facilityRepository.query(
+      `
+      SELECT
+        name,
+        numberoffloors,
+        numberofoffices,
+        numberofshops,
+        totalarea,
+        areaunit,
+        dimensions,
+        elevations,
+        description,
+        ST_AsGeoJSON(geom)::json AS geometry
+      FROM "cbd-buildings".facility;
+    `
+    );
+    return this.toGeoJSON(facilites);
   }
 
   findOne(id: number) {
